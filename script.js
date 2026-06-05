9c5ffc1aa1f48b39874816af03ddcee7dcf025f0
yudha33prawira <yudha33prawira@gmail.com>
2026-06-05 09:54:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ========== SUPABASE CONFIGURATION ==========
-const SUPABASE_URL = 'https://haylblhjzfavrfiyaicq.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://haylblhjzfavrfiyaicq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhheWxibGhqemZhdnJmaXlhaWNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MzgyMDIsImV4cCI6MjA5NTMxNDIwMn0.j4yQa1ZttP5_Zg0ye5lK2OLecq39QhG3tPyv5PZ3r78';
 
 // Inisialisasi Supabase dengan benar
