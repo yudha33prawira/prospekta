121fd7a5a7e80d2890c4de4ed2b34cf48166207b
yudha33prawira <yudha33prawira@gmail.com>
2026-06-19 14:44:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4067,7 +4067,7 @@ async function loadDbTransaksi() {
         query = query.eq('user_id', currentUser.id);
     }
     
-    const { data, error, count } = await query
+    const { data: uplineData, error: uplineError, count: uplineCount } = await query
             .order('created_at', { ascending: false })
             .range(0, 9999);
     
